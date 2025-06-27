7059f4f9a9a3928e86cb61076fd055ffe38ab990
Felix Mortensen <74263816+felixmortensen@users.noreply.github.com>
2025-06-27 20:30:35 +0200

    Update sero_seq_calculateBtensor.m

--- Sequence/sero_seq_calculateBtensor.m
+++ Sequence/sero_seq_calculateBtensor.m
@@ -1,5 +1,5 @@
 function B = sero_seq_calculateBtensor(seq)
-% function B = pulseq_calculateBtensor(seq)
+% function B = sero_seq_calculateBtensor(seq)
 %
 % Returns b-tensor in 1x6 "Voight-like" format
 % This function may not be general!
@@ -10,12 +10,12 @@ if ischar(seq) || isstring(seq)
     seq = tmp;
 end
 
-ind   = pulseq_getExcitationBlockInd(seq);
+ind   = sero_seq_getExcitationBlockInd(seq);
 nShot = numel(ind);
 B     = zeros(nShot, 6);
 
 for i = 1:nShot
-    [gwf, rf, dt] = pulseq_getShotWaveforms(seq, i);
+    [gwf, rf, dt] = sero_seq_getShotWaveforms(seq, i);
 
     tmp = fwf_gwf_to_btens(gwf, rf, dt, 2*pi()*seq.sys.gamma);
 
