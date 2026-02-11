c8403534cc95b3eccdbe154ac7925053af0f34ca
Felix Mortensen <felixmortensen1997@gmail.com>
2026-02-11 17:58:18 +0100

    Some update
    Some update

--- demo_rawdata2SRR.m
+++ demo_rawdata2SRR.m
@@ -7,7 +7,7 @@ fn_seq = 'SERO_example_sequence.seq';
 fn_xps = 'location of xps file';
 fn_nii = '2D_example.nii';
 
-sero_recon_data2nii_adaptiveCombine(fn_dat, fn_seq, fn_nii, fn_xps)
+sero_pulseq_data2nii_adaptiveCombine(fn_dat, fn_seq, fn_nii, fn_xps)
 
 %%
 
@@ -20,4 +20,4 @@ regstr = 1e-2;
 do_t1 = 1;
 mode = 1;
 
-sero_recon_nii2fit(fn_nii, fn_xps, mask_fn, regstr, do_t1, mode, new_nii)
+sero_srr_nii2fit(fn_nii, fn_xps, mask_fn, regstr, do_t1, mode, new_nii)
